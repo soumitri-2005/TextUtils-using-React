e0c907f128f413708b3f9d097c2aa9bee2b6c878
Soumitri Mishra <149056652+soumitri-2005@users.noreply.github.com>
2024-10-18 15:29:38 +0530

    Your commit message here

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Alert(props) {
   return (
-    <div style={{height: "50px"}}>
+    <div style={{height: "50px", width: "300px", margin: "auto", paddingTop: "10px"}}>
       {/* writting in this way bcz in useState the value has been assigned to null
       ${props.alert.type} for success i.e for green color  */}
       {props.alert && <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert"> 
