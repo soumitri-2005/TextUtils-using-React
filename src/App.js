903affd7d4f29e79dbffc8bc0e1d4b9474570e7e
Soumitri Mishra <149056652+soumitri-2005@users.noreply.github.com>
2024-10-21 19:25:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
-// so here i am using react router and i want to switch between my About and TextForm JS
-// import { 
-//   BrowserRouter as Router,
-//   Routes, 
-//   Route, 
-//   // Link 
-// } from 'react-router-dom';
-
 
 function App() {
   // adding theme and the reason adding this here bcz i can access the body of our page here directly
@@ -47,41 +38,6 @@ function App() {
   return (
     // we can use only single element so better to erite this in <>...</>
     <>
-      {/* <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">About</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-              <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav> */}
-
-      {/* here i called my componet */}
-      {/* so here i am making my props (title, abouttext....) and giving some values as my choice*/}
-      {/* <Router>
-        <Navbar title="TextUtiles" mode={mode} toggleMode={toggleMode} />
-        <Alert alert={alert} />
-        <div>
-          <Routes>
-            <Route exact path="/about" element={<About mode={mode}/>} />
-            <Route exact path="/" element={<TextForm showAlert={showAlert} mode={mode} />} />
-          </Routes>
-        </div>
-      </Router> */}
       <Navbar title="TextUtiles" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
       <div>
